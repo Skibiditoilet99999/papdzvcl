e11ffb19cdbc84aa5d1966145282188e8d5d1575
Soicodoc tool <phamthanh0962454457@gmail.com>
2025-11-30 16:23:57 +0700

    Add custom exception classes for PapdzVCL

---
+++ src/papdzvcl/exceptions.py
@@ -0,0 +1,11 @@
+class PapdzVCLException(Exception):
+    pass
+
+class Ok1Exception(PapdzVCLException):
+    pass
+
+class Ok2Exception(PapdzVCLException):
+    pass
+
+class Ok3Exception(PapdzVCLException):
+    pass
